f5c7a9ca3dd1c0ebe79cc9c6ed0ae2e3823b3b2e
Stefan Straka <stefan.straka@parlament.gv.at>
2024-12-01 14:43:08 +0100

    Initial commit

---
+++ src/main/kotlin/at/prewave/treechallenge/TreeChallengeApplication.kt
@@ -0,0 +1,11 @@
+package at.prewave.treechallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TreeChallengeApplication
+
+fun main(args: Array<String>) {
+    runApplication<TreeChallengeApplication>(*args)
+}
---
+++ src/test/kotlin/at/prewave/treechallenge/TreeChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package at.prewave.treechallenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TreeChallengeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
